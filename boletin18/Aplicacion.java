b4dd80c4602ab24cc4564c52680a7e48fc12913b
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-01-16 10:54:19 +0100

    Commit

--- boletin18/Aplicacion.java
+++ boletin18/Aplicacion.java
@@ -11,6 +11,13 @@ public class Aplicacion {
         Academia datos = new Academia("Carlos", nota, alumno);
 
         System.out.println(datos.toString());
+        
+        Persoal alumno2 = new Persoal(624522855, "correo2@gmail.com");
+        CalcularNota nota2 = new CalcularNota(11);
+        Academia datos2 = new Academia("Pepe", nota2, alumno);
+        
+        System.out.println(datos2.toString());
+        
     }
 
 }
